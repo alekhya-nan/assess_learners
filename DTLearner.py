41e2f60d186a74374b78f732cd1d9b6568e3afd6
alekhyanandula <32311062+alekhyanandula@users.noreply.github.com>
2023-08-27 22:33:01 -0700

    implemented pseudocode of DTLearner. Had to reshape to work with concat

--- DTLearner.py
+++ DTLearner.py
@@ -1,19 +1,12 @@
 import numpy as np
-
+import pandas as pd
 
 class DTLearner(object):
     def __init__(self, verbose=False):
-        """
-        Constructor method
-        """
-        pass  # move along, these aren't the drones you're looking for
+        self.verbose=verbose
 
     def author(self):
-        """
-        :return: The GT username of the student
-        :rtype: str
-        """
-        return "tb34"  # replace tb34 with your Georgia Tech username
+        return "anandula3"
 
     def add_evidence(self, data_x, data_y):
         """
@@ -25,14 +18,32 @@ class DTLearner(object):
         :type data_y: numpy.ndarray
         """
 
-        # slap on 1s column so linear regression finds a constant term
-        new_data_x = np.ones([data_x.shape[0], data_x.shape[1] + 1])
-        new_data_x[:, 0 : data_x.shape[1]] = data_x
+        concat_data = np.concatenate([data_x, data_y], axis=1)
+        print(concat_data.shape)
+        print(self.build_tree(concat_data))
 
-        # build and save the model
-        self.model_coefs, residuals, rank, s = np.linalg.lstsq(
-            new_data_x, data_y, rcond=None
-        )
+    def build_tree(self, data):
+        # if we're at a leaf node, then return 0
+        if data.shape[0] == 1:
+            return_leaf = np.array([-1, data[0][0], -1, -1])
+            return return_leaf.reshape((1, 4))
+        # if all y values are the same (compare to the first item in the y column), then return leaf node
+        if np.all(data[0,-1] == data[:,-1], axis = 0):
+            return_leaf = np.array([-1, data[0][0], -1, -1])
+            return return_leaf.reshape((1, 4))
+        
+        # determine best feature to split on
+        i = 0
+
+        
+        split_val = np.median(data[:,i])
+        left_tree = self.build_tree(data[data[:,i]<=split_val])
+        right_tree = self.build_tree(data[data[:,i]>split_val])
+
+        root = np.array([i, split_val, 1, left_tree.shape[0]+1])
+        root = root.reshape((1, 4))
+        concat_tree = np.concatenate([root, left_tree, right_tree], axis=0)
+        return concat_tree
 
     def query(self, points):
         """
@@ -49,4 +60,14 @@ class DTLearner(object):
 
 
 if __name__ == "__main__":
-    print("the secret clue is 'zzyzx'")
+    arr = pd.read_csv("Data/simple.csv", header=None).to_numpy()
+    print("simple shape", arr.shape)
+    x = arr[:,:2]
+    y = arr[:,-1]
+    y = y.reshape((len(y), 1))
+
+    print(x[:5], x.shape)
+    print(y[:5], y.shape)
+
+    dtlearner = DTLearner()
+    dtlearner.add_evidence(x, y)
