4c4d958cbdaa7199572b049434d0c52203cf1922
alekhyanandula <32311062+alekhyanandula@users.noreply.github.com>
2023-08-29 20:01:33 -0700

    added linter

--- DTLearner.py
+++ DTLearner.py
@@ -1,9 +1,10 @@
 import numpy as np
 
+
 class DTLearner(object):
     def __init__(self, leaf_size: int, verbose=False):
         self.leaf_size = leaf_size
-        self.verbose=verbose
+        self.verbose = verbose
 
     def author(self):
         return "anandula3"
@@ -24,27 +25,31 @@ class DTLearner(object):
         if np.allclose(data_y.flatten(), data_y[0][0], atol=0.000001):
             return_leaf = np.array([-1, data_y[0][0], -1, -1])
             return return_leaf.reshape((1, 4))
-        
+
         # determine best feature to split on
-        correlation = np.corrcoef(data_x, data_y, rowvar=False)[-1,:-1]
+        correlation = np.corrcoef(data_x, data_y, rowvar=False)[-1, :-1]
         correlation = np.abs(correlation)
         i = np.argmax(correlation)
-        split_val = np.median(data_x[:,i])
+        split_val = np.median(data_x[:, i])
 
         # building the left tree
-        left_split_condition = data_x[:,i]<=split_val
+        left_split_condition = data_x[:, i] <= split_val
 
         if np.sum(left_split_condition) == len(left_split_condition):
             leaf_pred = np.mean(data_y)
             return_leaf = np.array([-1, leaf_pred, -1, -1])
             return return_leaf.reshape((1, 4))
-        
-        left_tree = self.build_tree(data_x[left_split_condition], data_y[left_split_condition])
-        
-        right_split_condition = data_x[:,i]>split_val
-        right_tree = self.build_tree(data_x[right_split_condition], data_y[right_split_condition])
 
-        root = np.array([i, split_val, 1, left_tree.shape[0]+1]).reshape((1, 4))
+        left_tree = self.build_tree(
+            data_x[left_split_condition], data_y[left_split_condition]
+        )
+
+        right_split_condition = data_x[:, i] > split_val
+        right_tree = self.build_tree(
+            data_x[right_split_condition], data_y[right_split_condition]
+        )
+
+        root = np.array([i, split_val, 1, left_tree.shape[0] + 1]).reshape((1, 4))
         concat_tree = np.concatenate([root, left_tree, right_tree], axis=0)
 
         return concat_tree
@@ -58,12 +63,11 @@ class DTLearner(object):
 
         values = np.array(values)
 
-
         return values
-    
+
     def query_point(self, point):
         current_idx = 0
-        ## keep going while the 
+        ## keep going while the
         node = self.tree[0]
         while node[0] != -1:
             i = int(node[0])
@@ -78,8 +82,10 @@ class DTLearner(object):
 
         return node[1]
 
+
 def test_code():
     pass
+
 
 if __name__ == "__main__":
     test_code()
